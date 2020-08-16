778b5da828633d3aeef06bc6d30e855609307708
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-16 22:47:10 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
-var recipes = new O ({});
+var object = plqylist ({});
 
 var obj = { prop: 1 }
-function updateObjectWithKeyAndValue(object, key, value){
+function updateObjectWithKeyAndValue(playList, arti, value){
   return Object.assign({}, obj, {[key]: value})
 };
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
