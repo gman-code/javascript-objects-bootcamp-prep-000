24b09f34c81cad009a2e541a27d3f3db5300f0f8
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-16 22:40:42 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist={};
+var playlist={playlist}
+var playlist=updatedPlaylist();
+ function updatedPlaylist(playlist, artisName, songTitle){
+   playlist[artisName]=songTitle
+   return playlist
+ }
+function  destructivelyDeleteFromObjectByKey(Object,key ){
+  t Object()
+}
